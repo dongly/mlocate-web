f9af0f55a3e56a5de892b3847e0d2594bcb28442
dugite-code <dugite-code@peekread.info>
2019-09-17 14:41:28 +0800

    click to copy path

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from subprocess import Popen, PIPE
 from flask import send_from_directory
 from socket import gethostname
 from pipes import quote
-import glob, os
+import glob, os, hashlib
 
 MAX_RESULT_BYTES = os.getenv('MAX_RESULT_BYTES', 1000000)  # set to -1 to disable limit
 MAX_DATABASES = os.getenv('MAX_DATABASES', 5)
@@ -74,7 +74,10 @@ def index():
             results = results[:-1]
         resultslist = ""
         for entry in results:
-            resultslist = resultslist + "<li>" + entry.decode('utf-8') + "</li>"
+            thisresult = os.path.basename(entry).decode('utf-8')
+            thishash = hashlib.md5(thisresult.encode('utf-8')).hexdigest()
+            thispath = '&#92;' + entry.decode('utf-8').replace('/', '&#92;')
+            resultslist = resultslist + '<label class="file-label" for="' + thishash + '">' + thisresult + '</label><textarea readonly id="' + thishash + '" type="text" value="' + thispath + '" class="allowCopy noselect">' + thispath + '</textarea>'
 
     html = flask.render_template(
         'index.html',
@@ -89,6 +92,10 @@ def index():
 @app.route(APP_ROUTE + 'css/<path:path>')
 def send_css(path):
     return send_from_directory('css', path)
+
+@app.route(APP_ROUTE + 'js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
 
 if __name__ == '__main__':
     port = 5000
